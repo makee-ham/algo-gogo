9f945cb8a13639031c5f7734e092668d28e8db94
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-05-09 16:14:44 +0900

    [level 2] Title: 올바른 괄호, Time: 4.30 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,30 +1,24 @@
 function solution(s){
-    // ~생각...~
-    // 한 페어를 맞추려면 어케 해야하지.. ( 먼저 들어가고 ) 나중에...
-    // ) 가 (보다 먼저 나오는 게 있다면 암튼 그건 빠꾸임!!
-    // 근데 이게 스택이랑 무슨 상관이지??? 흐엉ㅠㅠㅠㅠ
-    // 뭔가 ( 가 홀로 나오면 +1 해주고 ( 또 나오면 +1 해주고 ) 나오면 -1 해주면서 0 되면 오케이?
+    // 다시 생각
+    // 그니까 저 문자열을 새 배열에 앞에서부터 넣어보자 이거야
+    // (는 배열에 넣고 )가 나오면 (를 이게... 뒤에서부터 괄호가 닫힐 테니 배열서 빼는겨
+    // 그랴서 ( 만 넣은 배열에 요소가 없으면 true 아이가
+    // 일단 해보자고
     
-    // 편의상 배열로 만들어주기
-    const sArr = [...s];
+    // 아 맞다 )가 더 많을 때도 처리해야 해
     
-    // ")"로 시작하면 걸러내기
-    const first = sArr[0];
-    if (first === ")") return false;
-    
-    // 짝 맞추기
-    let pairCount = 0;
-    for (let c of sArr) {
-        if (c === "(") {
-            pairCount++;
-        } else {
-            pairCount--;
-        }
-        
-        // 도중에 )가 더 많으면 그것도 짝 안 맞는 거임
-        if (pairCount < 0) return false;
+    const collector = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      collector.push("(");
+    } else {
+      if (collector.length === 0) {
+        return false;
+      }
+      collector.pop();
     }
-    
-    // 0이면 true, 아니면 짝이 안 맞는 거니까 false 반환
-    return pairCount === 0;
+  }
+
+  return collector.length === 0;
 }
