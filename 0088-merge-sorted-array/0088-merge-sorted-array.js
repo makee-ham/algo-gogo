2047bc06489cc34d194e3ee119ebd70653454452
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-06-11 19:21:38 +0900

    Time: 0 ms (100%), Space: 55 MB (48.72%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    // nums1 배열 뒤부터 넣기 (큰 순으로 nums1에 뒤부터 넣기)
+    let i = m - 1;
+    let j = n - 1;
+    let recentIdx = m + n - 1;
+
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[recentIdx--] = nums1[i];
+            i--;
+        } else {
+            nums1[recentIdx--] = nums2[j];
+            j--;
+        }
+    }
+};
